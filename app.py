b5c9d9e3702192e8fb345a9c7bed4d7c2bf5af89
Kait Nguyen <kaitnguyen@Kaits-MBP.attlocal.net>
2023-08-07 16:41:16 -0400

    updated starter

--- app.py
+++ app.py
@@ -1,30 +1,42 @@
 # Import the dependencies.
-
+import numpy as np
+import datetime as dt
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask,jsonify
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite://?Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+base = automap_base()
 
 # reflect the tables
-
-
+base.prepare(engine, reflect=True)
+base.classes.keys()
 # Save references to each table
-
+Measurement = base.classes.measurement
+Station = base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    
